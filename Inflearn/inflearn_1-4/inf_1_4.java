3feb1f7e0fca02f26db0c6a6f4aaf1551b2bfe86
juyohan <dygks8556@naver.com>
2021-05-06 15:06:33 +0900

    입력받은 문자열 모두 뒤집기

--- Inflearn/inflearn_1-4/inf_1_4.java
+++ Inflearn/inflearn_1-4/inf_1_4.java
@@ -1,5 +1,5 @@
 import java.io.*;
-//import java.util.*;
+import java.util.*;
 
 public class inf_1_4 {
 /*	public ArrayList<String> solution(int index, String[] text) {
@@ -14,7 +14,7 @@ public class inf_1_4 {
 	}
 */
 
-	public String solution(String text) {
+/*	public String solution(String text) {
 		char[] c = text.toCharArray();
 		String answer = "";
 
@@ -22,6 +22,28 @@ public class inf_1_4 {
 			answer += c[i];
 
 		return answer;
+	}
+*/
+
+	public ArrayList<String> solution(String[] text) {
+		ArrayList<String> strs = new ArrayList<>();
+
+		for (String str : text) {
+			char[] c = str.toCharArray(); // 문자열을 배열로
+			int st = 0;
+			int lt = str.length() - 1;
+			while (st < lt) {
+				char tmp = c[st];
+				c[st] = c[lt];
+				c[lt] = tmp;
+				st++;
+				lt--;
+			}
+			String tmp = String.valueOf(c); // 배열 -> 문자열로 변환
+			strs.add(tmp);
+		}
+
+		return strs;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -37,13 +59,20 @@ public class inf_1_4 {
 		if (index > 20 || index < 3)
 			System.out.println("3~20 사이의 수를 입력해주세요.");
 
-		for (int i = 0 ; i < index ; i++) {
+/*		for (int i = 0 ; i < index ; i++) {	
 			text[i] = br.readLine();
 			bw.write(inf.solution(text[i])+"\n");
 		}
+*/
+
+		for (int i = 0 ; i < index ; i++) 
+			text[i] = br.readLine();
 
 //		for (String str : inf.solution(index, text))
 //			bw.write(str);
+
+		for (String str : inf.solution(text))
+			bw.write(str + "\n");
 
 		bw.flush();
 		bw.close();
