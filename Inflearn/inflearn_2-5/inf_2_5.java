531315f689cb0f321311a581c3ebaf965f8d5919
juyohan <dygks8556@naver.com>
2021-05-21 14:49:57 +0900

    소수 찾기(에라토스테네스 체)

--- Inflearn/inflearn_2-5/inf_2_5.java
+++ Inflearn/inflearn_2-5/inf_2_5.java
@@ -2,14 +2,18 @@ import java.util.*;
 
 public class inf_2_5 {
 	public int solution(int count){
-		int answer = 1;
+		int answer = 0;
+		// 만약 값이 20이 들어오면 20까지 확인을 해야하는데 배열은 19까지 만들어지므로 +1을 해줘야한다.
+		int[] index = new int[count+1];
 
-		for (int i = 0; i <= count ; i++) {
-			for (int j = 2; j <= Math.sqrt(i) ; j++) {
-				if (i % j == 0)
-					break;
+		// 2부터 체크한다.
+		for (int i = 2; i <= count ; i++) {
+			if (index[i] == 0) {
+				// 해당 배열의 배수들을 다 체크해줌
+				for (int j = i ; j <= count ; j += i) 
+					index[j] = 1;
+				answer++;
 			}
-			answer++;
 		}
 
 		return answer;
