1f2be5fdd2d3ada0261518b005b7c109e66dda19
juyohan <dygks8556@naver.com>
2021-07-07 18:06:21 +0900

    카카오 크레인 인형뽑기

---
+++ Inflearn/inflearn_5-3/Main.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Main {
+	public Integer solution(int size, int[][] arr, int count, int[] countArr) {
+		Stack<Integer> stack = new Stack<>();
+		int answer = 0;
+
+		for (int i = 0 ; i < count ; i++) {
+			int num = countArr[i] - 1;
+			for (int j = 0 ; j < size ; j++) {
+				if (arr[j][num] != 0) {
+					if (stack.isEmpty())
+						stack.push(arr[j][num]);
+					else if (arr[j][num] == stack.peek()){
+						stack.pop();
+						answer += 2;
+					}
+					else if (arr[j][num] != stack.peek())
+						stack.push(arr[j][num]);
+					arr[j][num] = 0;
+					break;
+				}
+			}
+		}
+
+		return answer;
+
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		Scanner scan = new Scanner(System.in);
+
+		int size = scan.nextInt();
+		int[][] arr = new int[size][size];
+
+		for (int i = 0 ; i < size ; i++) {
+			for (int j = 0 ; j < size; j++) {
+				arr[i][j] = scan.nextInt();
+			}
+		}
+
+		int count = scan.nextInt();
+		int[] countArr = new int[count];
+		for (int i = 0 ; i < count ; i++) 
+			countArr[i] = scan.nextInt();
+
+		System.out.print(main.solution(size, arr, count, countArr));
+	}
+}
