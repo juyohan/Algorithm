44e73ff02cf7b473e1b40ab6573190039195c151
juyohan <dygks8556@naver.com>
2021-07-07 18:04:54 +0900

    매출액의 종류

---
+++ Inflearn/inflearn_4-3/Main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+	public ArrayList<Integer> solution(int[] value, int days, int k) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		ArrayList<Integer> answer = new ArrayList<>();
+
+		for (int i = 0 ; i < k ; i++) {
+			map.put(value[i], map.getOrDefault(value[i], 0) + 1);
+		}
+
+		int lt = 0;
+		answer.add(map.size());
+
+		for (int i = k ; i < days ; i++) {
+			map.put(value[i], map.getOrDefault(value[i], 0) + 1);
+			if (map.get(value[lt]) == 1)
+				map.remove(value[lt]);
+			else 
+				map.put(value[lt], map.get(value[lt]) -1);
+			lt++;
+			answer.add(map.size());
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		Scanner scan = new Scanner(System.in);
+
+		int days = scan.nextInt();
+		int k = scan.nextInt();
+		int value[] = new int[days];
+
+		for (int i = 0 ; i < days ; i++) 
+			value[i] = scan.nextInt();
+
+		for (int num : main.solution(value, days, k))
+			System.out.print(num + " ");
+	}
+}
