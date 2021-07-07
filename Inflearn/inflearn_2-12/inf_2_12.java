a815cb9c02df3315c99e08c63069847c55837304
juyohan <dygks8556@naver.com>
2021-07-07 18:02:51 +0900

    아직 못품

---
+++ Inflearn/inflearn_2-12/inf_2_12.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+
+public class inf_2_12 {
+	public Integer solution(int[][] inputs, int counts, int award) {
+		int answer = 0;
+		int[] student = new int[counts]
+
+		for (int i = 0 ; i < counts ; i++) {
+			for (int j = 0 ; j < award; j++) {
+				 // 안에 있는 학생이 i 등
+				for (int k = 
+				
+					
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		inf_2_12 inf = new inf_2_12();
+		Scanner scan = new Scanner(System.in);
+		int award = scan.nextInt(); // 안에 데이터 : 학생 이름
+		int counts = scan.nextInt();
+		int[][] inputs = new int[counts][award];
+
+		for (int i = 0 ; i < counts ; i++) {
+			for (int j = 0 ; j < award ; j++)
+				inputs[i][j] = scan.nextInt();
+		}
+
+		System.out.print(inf.solution(inputs, counts, award));
+	}
+}
