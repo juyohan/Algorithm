6de30bcf231e30bba998178f2e1749a2fb3a339c
juyohan <dygks8556@naver.com>
2021-07-07 18:05:21 +0900

    K번째 큰 수

---
+++ Inflearn/inflearn_4-5/Main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+	public int solution(int[] arr, int card, int pick) {
+		int answer = 0;
+		TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder());
+
+		for (int i = 0 ; i < card ; i++) {
+			for (int j = i + 1 ; j < card ; j++) {
+				for (int k = j + 1; k < card ; k++) {
+					set.add(arr[i] + arr[j] + arr[k]);
+				}
+			}
+		}
+		if (set.size() < pick)
+			return -1;
+		else {
+			Object[] value = set.toArray();
+			return (int) value[pick - 1];
+		}
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		Scanner scan = new Scanner(System.in);
+
+		int card = scan.nextInt();
+		int pick = scan.nextInt();
+		int arr[] = new int[card];
+		for (int i = 0 ; i < card ; i++) 
+			arr[i] = scan.nextInt();
+
+		System.out.print(main.solution(arr, card, pick));
+	}
+}
