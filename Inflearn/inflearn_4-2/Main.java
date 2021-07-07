b3bf1b4c6ced00c0c75dfcc3caa2f6073d9db90e
juyohan <dygks8556@naver.com>
2021-07-07 18:04:40 +0900

    아나그램

---
+++ Inflearn/inflearn_4-2/Main.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+	public String solution(String str1, String str2) {
+		String answer = "";
+
+		HashMap<Character, Integer> map1 = new HashMap<>();
+		HashMap<Character, Integer> map2 = new HashMap<>();
+
+		for (char c : str1.toCharArray()) {
+			map1.put(c, map1.getOrDefault(c, 0) +1);
+		}
+
+		for (char c : str2.toCharArray()) {
+			map2.put(c, map2.getOrDefault(c, 0) + 1);
+		}
+
+		if (map1.equals(map2) == true)
+			answer = "YES";
+		else 
+			answer = "NO";
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		Scanner scan = new Scanner(System.in);
+
+		String str1 = scan.next();
+		str1 = str1.toUpperCase();
+
+		String str2 = scan.next();
+		str2 = str2.toUpperCase();
+
+		System.out.print(main.solution(str1, str2));
+	}
+}
