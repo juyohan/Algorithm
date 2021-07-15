0b99b180f2b78f2de03dea081fbdd263d9eedc71
juyohan <dygks8556@naver.com>
2021-07-15 14:53:57 +0900

    이분 검색

--- Inflearn/inflearn_6-8/Main.java
+++ Inflearn/inflearn_6-8/Main.java
@@ -2,24 +2,43 @@ import java.util.*;
 
 public class Main {
 	public int solution(int num, int target, int[] arr) {
+		int answer = 0;
 		Arrays.sort(arr);
-
+/*
 		int a = num / 2 - 1;
 		if (arr[a] < target) {
 			for (int i = a ; i < num ; i++) {
 				if (arr[i] == target)
-					return i+1;
+					answer = i;
 			}
 		}
 		else if (arr[a] == target)
-			return a+1;
+			answer = a;
 		else {
 			for (int i = 0 ; i < a ; i++) {
 				if (arr[i] == target)
-					return i+1;
+					answer = i;
 			}
 		}
-		return -1;
+		return answer + 1;
+*/
+
+		int lt = 0;
+		int rt = num - 1;
+
+		while(lt <= rt) {
+			int mid = (lt + rt) / 2;
+			if (arr[mid] == target) {
+				answer = mid;
+				break;
+			}
+			if (arr[mid] > target)
+				rt = mid - 1;
+			else
+				lt = mid + 1;
+		}
+
+		return answer + 1;
 	}
 
 	public static void main (String[] argc) {
