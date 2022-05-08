5adb016ab4942ba605e23742c0b61b07c85f4504
juyohan <dygks8556@naver.com>
2022-05-08 22:39:04 +0900

    대소문자 변환

--- Inflearn/inflearn_1-2/inf_01_02.java
+++ Inflearn/inflearn_1-2/inf_01_02.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class inf_01_02 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class inf_01_02 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputString = br.readLine();
+        char[] stringArr = inputString.toCharArray();
+
+        String answer = "";
+        for (int i = 0; i < stringArr.length; i++) {
+            char target = stringArr[i];
+            if ('a' <= target && 'z' >= target)
+                answer += (char) ('A' + (target - 'a'));
+            else if ('A' <= target && 'Z' >= target)
+                answer += (char) ('a' + (target - 'A'));
+        }
+        System.out.print(answer);
+    }
 }
