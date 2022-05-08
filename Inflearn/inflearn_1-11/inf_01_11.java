93e3bfbbbfc768d85cf8eec3ff2cf55a909dd0bb
juyohan <dygks8556@naver.com>
2022-05-08 22:44:28 +0900

    문자열 압축

--- Inflearn/inflearn_1-11/inf_01_11.java
+++ Inflearn/inflearn_1-11/inf_01_11.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class inf_01_11 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class inf_01_11 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputString = br.readLine();
+
+        String answer = "";
+        int lt = 0, rt = 0, count = 0;
+        while (rt < inputString.length()) {
+            if (inputString.charAt(lt) == inputString.charAt(rt)) {
+                count++;
+                rt++;
+            }
+            else {
+                answer += inputString.charAt(lt);
+                if (count > 1)
+                    answer += count;
+                lt = rt;
+                count = 0;
+            }
+        }
+        answer += inputString.charAt(lt);
+        if (count > 1)
+            answer += count;
+        System.out.println(answer);
+    }
 }
