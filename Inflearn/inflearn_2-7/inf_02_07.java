c49a9aeb853a3e581b6543efbb0aaffe9463b15c
juyohan <dygks8556@naver.com>
2022-05-08 22:45:58 +0900

    점수 계산

--- Inflearn/inflearn_2-7/inf_02_07.java
+++ Inflearn/inflearn_2-7/inf_02_07.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class inf_02_07 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class inf_02_07 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] score = new int[N + 1];
+        score[0] = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < N + 1; i++) {
+            if (Integer.parseInt(st.nextToken()) == 1) {
+                score[i] = score[i - 1] + 1;
+            } else {
+                score[i] = 0;
+            }
+        }
+        System.out.print(Arrays.stream(score).sum());
+    }
 }
