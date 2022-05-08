6852ad14f5c89434d5aec53774758fba7af20023
juyohan <dygks8556@naver.com>
2022-05-08 22:43:47 +0900

    숫자만 추출

--- Inflearn/inflearn_1-9/inf_01_09.java
+++ Inflearn/inflearn_1-9/inf_01_09.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class inf_01_09 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class inf_01_09 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputString = br.readLine();
+
+        String numString = "";
+        for (int i = 0; i < inputString.length(); i++) {
+            if ('0' <= inputString.charAt(i) && inputString.charAt(i) <= '9')
+                numString += inputString.charAt(i);
+        }
+
+        System.out.print(Integer.parseInt(numString));
+    }
 }
