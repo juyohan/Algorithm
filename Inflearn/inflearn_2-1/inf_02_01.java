0160ff7165358f99f6a2d38a1c5096c3b52bcaa5
juyohan <dygks8556@naver.com>
2022-05-08 22:44:01 +0900

    큰 수 출력하기

--- Inflearn/inflearn_2-1/inf_02_01.java
+++ Inflearn/inflearn_2-1/inf_02_01.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class inf_02_01 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class inf_02_01 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        sb.append(arr[0] + " ");
+        for (int i = 1; i < N; i++) {
+            int target = arr[i];
+            if (target > arr[i - 1])
+                sb.append(target + " ");
+        }
+        System.out.print(sb);
+    }
 }
