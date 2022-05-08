e8103cde8e4429bdd9abe7a187441992746f0643
juyohan <dygks8556@naver.com>
2022-05-08 22:43:35 +0900

    유효한 팰린드롬

--- Inflearn/inflearn_1-8/inf_01_08.java
+++ Inflearn/inflearn_1-8/inf_01_08.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class inf_01_08 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class inf_01_08 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputString = br.readLine().toLowerCase();
+        int lt = 0, rt = inputString.length() - 1;
+
+        while (lt < rt) {
+            if (!Character.isAlphabetic(inputString.charAt(lt)))
+                lt++;
+            else if (!Character.isAlphabetic(inputString.charAt(rt)))
+                rt--;
+            else if (inputString.charAt(lt) != inputString.charAt(rt)) {
+                System.out.print("NO");
+                return;
+            } else if (inputString.charAt(lt) == inputString.charAt(rt)) {
+                rt--;
+                lt++;
+            }
+        }
+        System.out.print("YES");
+    }
 }
