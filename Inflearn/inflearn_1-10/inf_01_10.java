d8936eb37ee6f3cc415123298824c5e453f887a4
juyohan <dygks8556@naver.com>
2022-05-08 22:44:16 +0900

    가장 짧은 문자거리

--- Inflearn/inflearn_1-10/inf_01_10.java
+++ Inflearn/inflearn_1-10/inf_01_10.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class inf_01_10 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class inf_01_10 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String S1 = st.nextToken();
+        char c = st.nextToken().charAt(0);
+
+        int[] arr = new int[S1.length()];
+
+        int count = 1000;
+        for (int i = 0; i < S1.length(); i++) {
+            if (S1.charAt(i) == c)
+                count = 0;
+            else
+                count++;
+            arr[i] = count;
+        }
+
+        count = 1000;
+        for (int i = S1.length() - 1; i >= 0; i--) {
+            if (S1.charAt(i) == c)
+                count = 0;
+            else {
+                count++;
+                arr[i] = Math.min(arr[i], count);
+            }
+        }
+
+        for (int k : arr)
+            System.out.print(k + " ");
+    }
 }
