448595b7b11b3646f4652c381942fddab617a3d8
juyohan <dygks8556@naver.com>
2022-05-08 22:39:35 +0900

    문장 속 단어

--- Inflearn/inflearn_1-3/inf_01_03.java
+++ Inflearn/inflearn_1-3/inf_01_03.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class inf_01_03 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class inf_01_03 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputString = br.readLine();
+        String[] splitString = inputString.split(" ");
+
+        int max = -1, idx = 0;
+        for (int i = 0; i < splitString.length; i++) {
+            String target = splitString[i];
+            if (max < target.length()) {
+                max = target.length();
+                idx = i;
+            }
+        }
+
+        System.out.print(splitString[idx]);
+    }
 }
