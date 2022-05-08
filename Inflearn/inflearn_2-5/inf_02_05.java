a4b853892ed75fa6bfa57fc79015f53ec0a0905b
juyohan <dygks8556@naver.com>
2022-05-08 22:45:31 +0900

    소수 (에라토스테네스 체)

--- Inflearn/inflearn_2-5/inf_02_05.java
+++ Inflearn/inflearn_2-5/inf_02_05.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class inf_02_05 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class inf_02_05 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        boolean[] check = new boolean[N];
+
+        check[0] = check[1] = true;
+
+        for (int i = 2; i * i < N; i++) {
+            if (!check[i]) {
+                for (int j = i * i; j < N; j += i) check[j] = true;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < N; i++)
+            if (!check[i]) count++;
+
+        System.out.print(count);
+    }
 }
