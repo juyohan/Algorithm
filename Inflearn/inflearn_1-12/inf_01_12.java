a8c87041d550b1fbf3427df6b57f1e69506746ca
juyohan <dygks8556@naver.com>
2022-05-08 22:44:39 +0900

    암호

--- Inflearn/inflearn_1-12/inf_01_12.java
+++ Inflearn/inflearn_1-12/inf_01_12.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class inf_01_12 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class inf_01_12 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int charCount = Integer.parseInt(br.readLine());
+        String signal = br.readLine();
+
+        for (int i = 0; i < charCount; i++) {
+            String signalBit = signal.substring(0, 7);
+            StringBuilder sb = new StringBuilder(signalBit);
+            String reverseSignal = sb.reverse().toString();
+            signal = signal.substring(7);
+
+            int charNum = 0;
+            for (int k = 0; k < reverseSignal.length(); k++) {
+                if (reverseSignal.charAt(k) == '#')
+                    charNum += Math.pow(2, k);
+            }
+            System.out.print((char) charNum);
+        }
+    }
 }
