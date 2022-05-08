b33742e7401d647675e4ec23b831e48dd6dd7209
juyohan <dygks8556@naver.com>
2022-05-08 22:45:10 +0900

    피보나치 수열

--- Inflearn/inflearn_2-4/inf_02_04.java
+++ Inflearn/inflearn_2-4/inf_02_04.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class inf_02_04 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class inf_02_04 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        arr[0] = arr[1] = 1;
+        for (int i = 2; i < N; i++)
+            arr[i] = arr[i - 2] + arr[i - 1];
+
+        for (int k : arr)
+            System.out.print(k + " ");
+    }
 }
