86f2ee80ea9a6d9c0d33f8229a59e2ad9a2876c3
juyohan <dygks8556@naver.com>
2022-05-08 22:40:56 +0900

    막대기

---
+++ BJ/BJ_1094/BJ_1094.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_1094 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int branch = 64, cnt = 0;
+
+        while (branch > 0) {
+            if (branch <= X) {
+                X -= branch;
+                cnt++;
+            }
+            if (X == 0) {
+                break;
+            }
+            branch /= 2;
+        }
+        System.out.println(cnt);
+    }
+}
