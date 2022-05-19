297bb397ff1ee4fa6c3764f3f0854ef78c1ce4fd
juyohan <dygks8556@naver.com>
2022-05-19 21:48:38 +0900

    응급실

--- Inflearn/inflearn_5-8/inf_05_08.java
+++ Inflearn/inflearn_5-8/inf_05_08.java
@@ -1,20 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class inf_05_08_Node implements Comparable<inf_05_08_Node> {
-    int number;
-    int value;
+class inf_05_08_Node {
+    int number, value;
 
     inf_05_08_Node(int number, int value) {
         this.number = number;
         this.value = value;
-    }
-
-    @Override
-    public int compareTo(inf_05_08_Node o) {
-        return this.value - o.value;
     }
 }
 
@@ -22,40 +18,38 @@ public class inf_05_08 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-
-        int[] patients = new int[N];
+        int[] arr = new int[N];
         for (int i = 0; i < N; i++)
-            patients[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
 
-        System.out.print(solution(M, patients));
+        System.out.print(solution(M, arr));
     }
 
-    public static int solution(int M, int[] patients) {
+    public static int solution(int M, int[] arr) {
         int answer = 0;
         Queue<inf_05_08_Node> queue = new LinkedList<>();
 
-        for (int i = 0 ; i < patients.length; i++)
-            queue.add(new inf_05_08_Node(i, patients[i]));
+        for (int i = 0; i < arr.length; i++)
+            queue.add(new inf_05_08_Node(i, arr[i]));
 
         while (!queue.isEmpty()) {
             inf_05_08_Node now = queue.poll();
+            boolean check = false;
 
             for (inf_05_08_Node next : queue) {
-                if (now.value < next.value) {
+                if (next.value > now.value) {
                     queue.add(now);
+                    check = false;
                     break;
-                }
+                } else check = true;
             }
-
-            if (!queue.contains(now)) {
-                answer++;
-                if (now.number == M) break;
-            }
+            if (!check) continue;
+            answer++;
+            if (now.number == M) break;
         }
 
         return answer;
