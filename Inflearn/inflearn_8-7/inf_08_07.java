85a26935d599d46e2844c42292f0f416881bd24b
juyohan <dygks8556@naver.com>
2022-06-06 17:28:52 +0900

    조합의 경우수

--- Inflearn/inflearn_8-7/inf_08_07.java
+++ Inflearn/inflearn_8-7/inf_08_07.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class inf_08_07 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class inf_08_07 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        System.out.print(solution(n, r));
+    }
+
+    static int answer = 0;
+
+    public static int solution(int n, int r) {
+        int[][] maps = new int[n + 1][r + 1];
+
+        answer = DFS(n, r, maps);
+
+        return answer;
+    }
+
+    static int DFS(int n, int r, int[][] maps) {
+        if (maps[n][r] > 0) return maps[n][r]; // memorization
+        if (n == r || r == 0) return 1;
+        else return maps[n][r] =  DFS(n - 1, r - 1, maps) + DFS(n - 1, r, maps);
+    }
 }
--- Inflearn/inflearn_8-7/inf_08_07_Re.java
+++ Inflearn/inflearn_8-7/inf_08_07_Re.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class inf_08_07_Re {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class inf_08_07_Re {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        System.out.print(solution(n, r));
+    }
+
+    static int solution(int n, int r) {
+        int answer = 0;
+        int[][] map = new int[n + 1][r + 1];
+
+        answer = DFS(n, r, map);
+
+        return answer;
+    }
+
+    static int DFS(int n, int r, int[][] map) {
+        if (map[n][r] > 0) return map[n][r];
+        if (n == r || r == 0) return 1;
+        else {
+            return map[n][r] = DFS(n - 1, r - 1, map) + DFS(n - 1, r, map);
+        }
+    }
 }
