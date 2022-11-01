a7d9232acc2fa5f14184c2e5cba432f970bbd6f3
juyohan <dygks8556@naver.com>
2022-11-02 00:14:40 +0900

    방문처리 추가

--- BJ/BJ_2098/BJ_2098s.java
+++ BJ/BJ_2098/BJ_2098s.java
@@ -7,38 +7,48 @@ import java.io.InputStreamReader;
 public class BJ_2098s {
     static int[][] arr;
     static int[][] dp;
-    static int N, INF = Integer.MAX_VALUE - 1000000;
+    static int N, CANNOT_CYCLE = 16 * 1000000 + 1, NOT_VISIT = CANNOT_CYCLE * 2;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         arr = new int[N][N];
-        dp = new int[N][(1 << N)];
+        dp = new int[N][1 << N];
 
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++)
                 arr[i][j] = Integer.parseInt(st.nextToken());
 
-            Arrays.fill(dp[i], INF); // 모든 값을 INF 로 채움
+            Arrays.fill(dp[i], NOT_VISIT); // 방문 안한 값으로 채움
         }
     
         System.out.println(solution(0, 1));
+        for (int[] items : dp) {
+            for (int item : items) {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static int solution(int current, int visited) {
+        visited = visited | 1 << current; // 방문처리
+
         if (visited == (1 << N) - 1) { // 모든 도시를 방문 완료했을 때
-            if (arr[current][0] == 0) 
-                return INF;
-            return arr[current][0]; // 마지막 도시로 돌아오기 위한 비용
+            if (arr[current][0] == 0)  // 마지막 도시에서 처음 도시로 못 돌아갈 때
+                return CANNOT_CYCLE;
+            return arr[current][0]; // 처음 도시로 돌아오기 위한 비용
         }
         
-        if (dp[current][visited] != INF) { // 이미 방문을 했다면, 해당 값 반환
+        if (dp[current][visited] != NOT_VISIT) { // 이미 방문을 했다면, 해당 값 반환
             return dp[current][visited];
         }
 
         for (int i = 0 ; i < N ; i++) {
-            if (arr[current][i] != 0 && (visited & (1 << i)) == 0)
-                dp[current][visited] = Math.min(dp[current][visited], solution(i, visited | (1 << i)) + arr[current][i]);
+            if (arr[current][i] != 0 && (visited & (1 << i)) == 0) {// 다음 도시로 갈 수 있고, 방문하지 않았을 경우
+                dp[current][visited] = Math.min(dp[current][visited], solution(i, visited) + arr[current][i]);
+            }
         }
 
         return dp[current][visited];
