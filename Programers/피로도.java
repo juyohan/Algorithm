ab253cf6b08f1daf48daf94e6731fcf4ee010f1e
juyohan <dygks8556@naver.com>
2022-11-05 17:42:30 +0900

    피로도

---
+++ Programers/피로도.java
@@ -0,0 +1,21 @@
+﻿class Solution {
+    public static int[] visited;
+    public static int answer = 0;
+    
+    public int solution(int k, int[][] dungeons) { 
+        visited = new int[dungeons.length];
+        dfs(0, k, dungeons);
+        return answer;
+    }
+    
+    public void dfs(int start, int k, int[][] dungeons) {
+        for (int i = 0 ; i < dungeons.length; i++) {
+            if (visited[i] == 0 && k >= dungeons[i][0]) {
+                visited[i] = 1;
+                dfs(start + 1, k - dungeons[i][1], dungeons);
+                visited[i] = 0;
+            }
+        }
+        answer = Math.max(answer, start);
+    }
+}
