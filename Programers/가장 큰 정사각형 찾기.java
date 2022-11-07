23ccf92f947642efe9fa0470fea8209deb3db434
juyohan <dygks8556@naver.com>
2022-11-08 00:15:13 +0900

    가장 큰 정사각형 찾기

---
+++ Programers/가장 큰 정사각형 찾기.java
@@ -0,0 +1,25 @@
+﻿import java.lang.Math;
+
+class Solution {
+    static int[][] dp;
+    
+    public int solution(int [][]board) {
+        int max = 0;
+        
+        if (board.length == 1 || board[0].length == 1)
+            return 1;
+        
+        dp = new int[board.length + 1][board[0].length + 1];
+    
+        for (int i = 1 ; i < board.length ; i++) {
+            for (int j = 1 ; j < board[i].length; j++) {
+                if (board[i][j] != 0) {
+                    board[i][j] = Math.min(board[i - 1][j], Math.min(board[i - 1][j - 1], board[i][j - 1])) + 1;
+                }
+                max = Math.max(board[i][j], max);
+            }
+        }
+
+        return max * max;
+    }
+}
