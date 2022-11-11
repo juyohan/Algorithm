ecada587996e40c75e2e15a1207c0b20774a7ba9
juyohan <yohan.ju@kt.com>
2022-11-11 17:02:27 +0900

    그대로 출력하기 2

---
+++ BJ/BJ_11719/BJ_11719.java
@@ -0,0 +1,15 @@
+package BJ.BJ_11719;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_11719 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+            while((str = br.readLine()) != null) {
+                System.out.println(str);
+            }
+    }
+}
