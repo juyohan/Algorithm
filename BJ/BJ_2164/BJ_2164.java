d90911ad0688cf9b8e2f69ed019ee853df5f7afc
juyohan <dygks8556@naver.com>
2022-11-18 00:23:08 +0900

    카드2

---
+++ BJ/BJ_2164/BJ_2164.java
@@ -0,0 +1,32 @@
+﻿import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BJ_2164 {
+    static Queue<Integer> queue = new LinkedList<>();
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(br.readLine());
+
+            for (int i = 1 ; i <= N ; i++) {
+                queue.offer(i);
+            }
+
+            System.out.println(solution());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static int solution() {
+        if (queue.size() == 1)
+            return queue.poll();
+
+        queue.poll();
+        int peek = queue.poll();
+        queue.offer(peek);
+
+        return solution();
+    }
+}
