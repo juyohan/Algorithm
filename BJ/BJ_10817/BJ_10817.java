18e03983e34c0df1f457955c37d379aa5c922a96
juyohan <dygks8556@naver.com>
2022-11-20 19:20:47 +0900

    세 수

---
+++ BJ/BJ_10817/BJ_10817.java
@@ -0,0 +1,22 @@
+﻿import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BJ_10817 {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String str = br.readLine();
+
+            solution(str);
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    public static void solution(String str) {
+        int[] arr = Arrays.stream(str.split(" ")).mapToInt(x -> Integer.parseInt(x)).toArray();
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+    }
+}
