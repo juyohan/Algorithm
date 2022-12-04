63270dfcc57e365d8bb7a3fcc807fb6ac3f52edd
juyohan <dygks8556@naver.com>
2022-12-04 19:36:09 +0900

    마지막 숫자에 1 더하기

---
+++ LeetCode/plus_one.java
@@ -0,0 +1,31 @@
+﻿package LeetCode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class plus_one {
+    public int[] plusOne(int[] digits) {
+        List<Integer> digitList = new ArrayList<>();
+        digits[digits.length - 1] += 1; // 배열의 마지막 숫자에 1 더함
+        
+        digitList = sort(digits, 1, 0, digitList);
+        Collections.reverse(digitList);
+        
+        return digitList.stream().mapToInt(Integer::intValue).toArray();
+    }
+    
+    public List<Integer> sort(int[] digits, int idx, int carry, List<Integer> digitList) {
+        int targetIdx = digits.length - idx++;
+        if (targetIdx < 0) {
+            if (carry > 0) {
+                digitList.add(carry);
+            }
+            return digitList;
+        }
+        digits[targetIdx] += carry;
+        digitList.add(digits[targetIdx] % 10);
+        return sort(digits, idx, digits[targetIdx] / 10, digitList);
+    }
+    
+}
